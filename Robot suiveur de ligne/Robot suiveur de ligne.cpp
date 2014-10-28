50e5b71f1f6969e26c82bb22ace8c02b9c4652eb
adri <adrienvandmme94@gmail.com>
2014-10-28 18:55:44 +0100

    PWM test et fonctionelle

--- Robot suiveur de ligne/Robot suiveur de ligne.cpp
+++ Robot suiveur de ligne/Robot suiveur de ligne.cpp
@@ -17,6 +17,9 @@
 #include "fonction.h"
 
 LCD lcd;
+uint16_t analog_old = 512;
+uint16_t analog_new;
+int duty;
 int main(void)
 {
 
@@ -24,15 +27,23 @@ int main(void)
 	DDRB |= _BV(PINB5);
 	lcd.display_init();
 	lcd.clear();
-	if(e_read(0x0000) != 1 )//check first startup
+	if(e_read(0x0000) != 1 && 1==2)//check first startup
 	{
 		setup();	
 	}
     while(1)
     {
 		lcd.set_cursor(0,1);
-		lcd.print_text("set-up ok");
+		lcd.print_text("          ");
+		lcd.set_cursor(0,1);
 		if((PINC&(1<<BP_PIN)))
 			setup();
+		analog_new = analog_read(1, analog_old); //Attention, il faut positioner le curseur du pot à 50% pour démarer !
+		if(analog_new <=1024)
+			analog_old = analog_new;
+		duty = analog_old/10.24;
+		pwm_timer1(duty, 5);
+		lcd.print_number(duty);
+		_delay_ms(20);
     }
 }
